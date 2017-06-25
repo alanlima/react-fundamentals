d71de870ae28326eed79f66334d5aec55dc98a55
Alan Lima <alima@outlook.com>
2017-06-25 23:41:10 +1000

    - added .DS_Store to .gitignore - added Axios component for Ajax request - updated the Popular page to show a grid with the repositories for the selected language

--- app/components/popular.js
+++ app/components/popular.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Api from '../utils/api';
 
 function SelectLanguage (props) {
     var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -25,20 +26,63 @@ SelectLanguage.propTypes = {
     onSelect: PropTypes.func.isRequired
 }
 
+function RepoGrid(props){
+    return (
+        <ul className='popular-list'>
+            {props.repos.map(function(repo, index){
+                return (
+                    <li key={repo.name} className='popular-item'>
+                        <div className='popular-rank'>#{index + 1}</div>
+                        <ul className='space-list-items'>
+                            <li>
+                                <img
+                                    className='avatar'
+                                    src={repo.owner.avatar_url}
+                                    alt={'Avatar for ' + repo.owner.login} 
+                                />
+                            </li>
+                            <li><a href={repo.html_url}>{repo.name}</a></li>
+                            <li>@{repo.owner.login}</li>
+                            <li>{repo.stargazers_count} starts</li>
+                        </ul>
+                    </li>
+                )
+            })}
+        </ul>
+    );
+}
+
+RepoGrid.propTypes = {
+    repos: PropTypes.array.isRequired
+}
+
 class Popular extends Component {
     constructor(props){
         super(props);
         this.state = {
-            selectedLanguage: 'All'
+            selectedLanguage: 'All',
+            repos: null
         };
 
         this.updateLanguage = this.updateLanguage.bind(this);
     }
 
+    componentDidMount () {
+      this.updateLanguage(this.state.selectedLanguage);
+    }
+
     updateLanguage(lang) {
         this.setState({
-            selectedLanguage: lang
+            selectedLanguage: lang,
+            repos: null
         });
+
+        Api.fetchPopularRepos(lang)
+            .then(function(repos){
+                this.setState({
+                    repos: repos
+                })
+        }.bind(this));
     }
 
     render(){
@@ -49,6 +93,9 @@ class Popular extends Component {
                     selectedLanguage={this.state.selectedLanguage}
                     onSelect={this.updateLanguage}
                 />
+                {this.state.repos ?
+                    <RepoGrid repos={this.state.repos} /> :
+                    <p>Loading...</p>}
             </div>
         );
     }
---
+++ app/utils/api.js
@@ -0,0 +1,16 @@
+import Axios from 'axios';
+
+module.exports = {
+    fetchPopularRepos: function(language) {
+        var encodedURI = window.encodeURI(
+            'https://api.github.com/search/repositories?q=stars:>1+language:' +
+            language +
+            '&sort=star&order=desc&type=Repositories'
+        );
+
+        return Axios.get(encodedURI)
+            .then(function(response){
+                return response.data.items;
+            });
+    }
+};
