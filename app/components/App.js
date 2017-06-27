6bff2c23a763287265838c1a9a4fb03e7a47af4e
Alan Lima <alima@outlook.com>
2017-06-27 13:10:33 +1000

    added routers to the site and created the battle, home pages and the navbar

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-import Popular from './popular'
+import Popular from './popular';
+import Home from './home';
+import Battle from './battle';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Nav from './nav';
 
 class App extends Component {
     render(){
         return (
-            <div className="container">
-                <Popular />
-            </div>
+            <BrowserRouter>
+                <div className="container">
+                    <Nav />
+                    
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path='/popular' component={Popular} />
+                        <Route exact path='/battle' component={Battle} />
+                        <Route render={function(){
+                            return <p>Not Found</p>;
+                        }} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
             )
     }
 }
---
+++ app/components/Battle.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Battle extends Component {
+    render() {
+        return (
+            <div> Battle Page </div>
+        );
+    }
+}
+
+module.exports = Battle;
