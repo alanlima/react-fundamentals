930bf670b12577735f2dae109ee59259791cd8b9
Alan Lima <alima@outlook.com>
2017-07-02 15:16:06 +1000

    added the battle logic to the api

--- app/utils/api.js
+++ app/utils/api.js
@@ -15,9 +15,51 @@ function getRepos (username){
     return Axios.get('https://api.github.com/users/' + username);
 }
 
+function getStarCount(repos) {
+    return repos.data.reduce(function(count, repo){
+        return count + repo.stargazers_count;
+    }, 0);
+}
+
+function calculateScore(profile, repos) {
+    const followers = profile.followers;
+    const totalStars = getStarCount(repos);
+
+    return (followers * 3) + totalStars;
+}
+
+function handleError(error) {
+    console.warn(error);
+    return null;
+}
+
+function getUserData(username) {
+    return Axios.all([
+        getProfile(username),
+        getRepos(username)
+    ]).then(function(data){
+        const profile = data[0];
+        const repos = data[1];
+
+        return {
+            profile: profile,
+            score: calculateScore(profile, repos)
+        };
+    });
+}
+
+function sortPlayers(players){
+    return players.sort(function(a, b){
+        return b.score - a.score;
+    });
+}
+
 module.exports = {
     battle: function(players) {
-
+        return Axios
+            .all(players.map(getUserData))
+            .then(sortPlayers)
+            .catch(handleError);
     },
 
     fetchPopularRepos: function(language) {
